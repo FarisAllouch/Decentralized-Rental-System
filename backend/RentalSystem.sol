f06a58b4c113167e5782349c1da449f06dc98e0c
FarisAllouch <allouchfaris21@gmail.com>
2025-01-13 23:53:02 +0100

    fix

--- backend/RentalSystem.sol
+++ backend/RentalSystem.sol
@@ -2,32 +2,62 @@
 pragma solidity 0.8.28;
  
 contract SimpleElection {
-    struct Candidate {
-        uint id;
-        string name;
-        uint votes;
+
+    struct Property {
+            uint id;
+            string landlordName;
+            string apartmentType;
+            uint totalArea;
+            string location;
+            uint rentPerMonth;
     }
- 
+
+    struct Feedback {
+    address sender;
+    string message;
+    }
+
+    Property[] public properties;
+    Feedback[] public feedbacks;
+
+    uint public nextId = 1;
+    address[] public pendingRequests;
     address public owner;
 
     mapping(address => bool)public landlords;
 
-    address[] public pendingRequests;
-
     event AccessRequested(address indexed user);
     event AccessGranted(address indexed user);
     event AccessRevoked(address indexed user);
+    event InterestShown(address indexed tenant, uint256 propertyId);
  
     constructor() {
         owner = msg.sender;
     }
 
-      function requestAccess() public {
-        require(!landlords[msg.sender], "Already a landlord.");
-        require(!_isPending(msg.sender), "Access request already pending.");
-        
-        pendingRequests.push(msg.sender);
-        emit AccessRequested(msg.sender);
+    function submitFeedback(string memory _message) public {
+    feedbacks.push(Feedback(msg.sender, _message));
+    }
+
+    function getFeedbacks() public view returns (Feedback[] memory) {
+        return feedbacks;
+    }
+    
+    function addProperty(string memory _landlordName, string memory _apartmentType, uint _totalArea, string memory _location, uint _rentPerMonth) public {
+    properties.push(Property(nextId, _landlordName, _apartmentType, _totalArea, _location, _rentPerMonth));
+    nextId++;
+    }
+
+    function getProperties() public view returns (Property[] memory) {
+    return properties;
+    }
+
+    function requestAccess() public {
+    require(!landlords[msg.sender], "Already a landlord.");
+    require(!_isPending(msg.sender), "Access request already pending.");
+    
+    pendingRequests.push(msg.sender);
+    emit AccessRequested(msg.sender);
     }
 
     function grantAccess(address _user) public {
